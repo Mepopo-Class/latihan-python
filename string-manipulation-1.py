4564217d03c6c7996657daed4a34a95044a6138b
Ivan Nircahya <ivanxirko1987@gmail.com>
2021-04-26 16:42:07 +0700

    Soal Baru: string manipulation

---
+++ string-manipulation-1.py
@@ -0,0 +1,14 @@
+# < Soal >
+# Loop lah string berikut menjadi terbalik
+
+# < Ketentuan >
+# reverse string berikut
+# buat variabel terpisah untuk wadah jawaban
+# ambil per huruf dan incrementkan pada variabel wadah
+# cara ambil per huruf.. nama_variabel[index]
+# output yg diharapkan:
+  # sucsibiH
+
+# < Tips >
+# Gunakan kembali teknik yg pernah digunakan pada soal loop array terbalik (loop-3)
+# gunakan concate string += dan assign nilainya ke variabel wadah
---
+++ string-manipulation-3.py
@@ -0,0 +1,13 @@
+# < Soal >
+# Ambil judul postingan pada path berikut
+
+# < Ketentuan >
+# gunakanlah built-in function dan method python
+# pada soal ini kita pakai method .split(), baca:
+# https://www.w3schools.com/python/ref_string_split.asps
+
+# < Tips >
+# pay $999 to unlock tips..
+
+
+path = "postingan/trending/cara-bernafas"
