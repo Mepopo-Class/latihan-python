77e1563c6ddf92612aa393914a28f51126c65b52
Ivan Nircahya <ivanxirko1987@gmail.com>
2021-05-07 21:55:43 +0700

    new soal: function

---
+++ function-1.py
@@ -0,0 +1,17 @@
+# < Soal >
+# buat function yang dapat memanipulasi variabel global
+
+# < Ketentuan >
+# buatlah function untuk memanipulasi variabel "x" berikut
+# terserah mau tambah, kurang, kali, dll (yang penting data berubah)
+# panggil function sebanyak 2 kali (atau lebih) 
+# untuk melihat apakah benar variabel global sudah dimanipulasi
+
+# semisal kalo kita buat function "tambahkan" 
+# dengan fungsionalitasnya menambahkan nilai var dengan +5, maka:
+# < Output >
+# 15
+# 20
+
+
+x = 10
---
+++ function-2.py
@@ -0,0 +1,19 @@
+# < Soal >
+# Cari rata rata dari data dalam array berikut!
+
+# < Ketentuan >
+# buatlah function untuk menjumlahkan semua data pada array berikut
+# setelah dijumlahkan semua maka bagi hasil tersebut dengan banyak data, cari: "rumus rata-rata"
+# print hasilnya terletak diluar function
+# jadi function hanya digunakan untuk menjumlahkan data lalu mengirim(return) hasilnya
+
+# < Tips >
+# buatlah variabel kosong sebagai wadah untuk hasil penjumlahan seluruh datanya
+# gunakan looping untuk menjumlahkan
+# pastikan untuk me-return-kan hasil tsb
+
+# < Output >
+# Rata-rata: 16.285714285714285
+
+
+angkaku = [12, 24, 14, 13, 12, 20, 19]
