a2bea638fa5658e809dda3eef82e6fd9e6efae56
Christyan-Cyan <christyananugrah2801@gmail.com>
2021-05-09 23:01:32 +0700

    Menyelesaikan Soal Function 2

---
+++ function-2.py
@@ -0,0 +1,34 @@
+# < Soal >
+# Cari rata rata dari data dalam array berikut!
+
+# < Ketentuan >
+# buatlah function untuk menjumlahkan semua data pada array berikut
+# setelah dijumlahkan semua maka bagi hasil tersebut dengan banyak data, cari: "rumus rata-rata"
+# print hasilnya terletak diluar function
+# jadi function hanya digunakan untuk menjumlahkan data lalu mengirim(return) hasilnya
+
+# < Tips >
+# buatlah variabel kosong sebagai wadah untuk hasil penjumlahan seluruh datanya
+# gunakan looping untuk menjumlahkan
+# pastikan untuk me-return-kan hasil tsb
+
+# < Output >
+# Rata-rata: 16.285714285714285
+
+
+angkaku = [12, 24, 14, 13, 12, 20, 19]
+
+
+def avrg(angkaku):
+    jumlah = 0
+    angka = angkaku
+    # jumlah = sum(angkaku) (Bisa pakai cara Ini, tapi takut ipan marah)
+    for i in angkaku:
+        jumlah += i
+    return jumlah
+
+
+jumlah_total = avrg(angkaku)
+print("Jumlah Keseluruhan data adalah", jumlah_total)
+hasil = jumlah_total / len(angkaku)
+print("Rata rata nya adalah", hasil)
